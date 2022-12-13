ac92457caffce3d439b42f9dbf896bc309848547
Patrick Fernando <patrickfernando1234@gmail.com>
2022-12-13 16:55:25 -0300

    first commit

---
+++ Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApplication9.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+        
+        public Product (string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }        
+    }
+}
